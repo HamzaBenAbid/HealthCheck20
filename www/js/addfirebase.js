87804e2371fef9d4d9a6074b3f773fd2a48869d9
hamza ben abid <hamzabozambo@gmail.com>
2020-12-26 21:12:35 +0100

    fire base eror

---
+++ www/js/addfirebase.js
@@ -0,0 +1,46 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+         
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+$('#signupsub').click(function () {
+    var nom = $('#fullname').val();
+    var email = $('#email2').val();
+    var password = $('#password2').val();
+    
+    //Créer une référence à la base de données
+    var firebaseRef = firebase.database().ref();
+    if ((nom != "") && (email != "")(password != "")){
+        var noeud = firebaseRef.push();
+        
+        noeud.child("nom").set(nom);
+        noeud.child("email").set(email);
+        noeud.child("password").set(password);
+    }
+    
+});
