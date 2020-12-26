bd2b9e5cd51d7927c318a9ef5aeafe206ffd4cab
hamza ben abid <hamzabozambo@gmail.com>
2020-12-26 17:55:23 +0100

    add plugin

--- www/js/addplugin.js
+++ www/js/addplugin.js
@@ -2,10 +2,10 @@ const { takePicture } = require("../../plugins/cordova-plugin-camera/src/browser
 
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-    document.getElementById('upload').addEventListener('click',takePicture);
+    document.getElementById('Uploadpicture').addEventListener('click',takePicture);
 
     function takePicture(){
-        navigator.camera.getPicture(success,console.error{
+        navigator.camera.getPicture(success,console.error,{
             quality:50,
             destinationType:camera.DestinationType.Data_URL
         });
